7be10e73b84c1e2ad5157ffcc88b743248b1b1bf
zubxir <zoomohammmed@gmail.com>
2019-11-05 17:39:55 -0600

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+676.times do
+  Product.create(
+    title: Faker::Coffee.blend_name,
+    price: Faker::Commerce.price,
+    stock_quantity: Faker::Number.number
+  )
+end
+
+puts Product.count
